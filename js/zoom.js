a76fdad269470847e4e97e5f6e1dd2ac407b25dc
volodymyrkorobkin <6040683@mborijnland.nl>
2024-02-13 13:21:26 +0100

    Add zoom future

---
+++ js/zoom.js
@@ -0,0 +1,53 @@
+// zoom.js
+// Een script dat ervoor zorgt dat een afbeelding kan worden vergroot door erop te klikken.
+// Gemaakt door: Volodymyr Korobkin
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var imgs = document.querySelectorAll('.zoom');
+    imgs.forEach(function(img) {
+        img.addEventListener('click', function() {
+            if (img.classList.contains('zoomed')) {
+                img.classList.remove('zoomed');
+            } else {
+                var newImg = document.createElement('img');
+                newImg.classList.add('img-overlay');
+
+                newImg.src = img.src;
+                newImg.style.top = img.offsetTop - window.scrollY + 'px';
+                newImg.style.left = img.offsetLeft - window.scrollX + 'px';
+                newImg.style.width = img.width + 'px';
+                newImg.style.height = img.height + 'px';
+
+                document.body.appendChild(newImg);
+                img.style.opacity = '0';
+
+
+                setTimeout(function() {
+                    newImg.classList.add('zoomed');
+                    newImg.style.top = '0';
+                    newImg.style.left = '0';
+
+                    newImg.style.width = '100%';
+                    newImg.style.height = '100%';
+
+                    newImg.addEventListener('click', function() {
+                        newImg.classList.remove('zoomed');
+                        newImg.style.top = img.offsetTop - window.scrollY + 'px';
+                        newImg.style.left = img.offsetLeft - window.scrollX + 'px';
+
+                        newImg.style.width = img.width + 'px';
+                        newImg.style.height = img.height + 'px';
+
+
+                        setTimeout(function() {     
+                            img.style.opacity = '1';
+                            newImg.remove();
+                        }, 450);
+                    });
+                }, 1);
+            }
+        });
+    });
+});
--- pages/home.php
+++ pages/home.php
@@ -12,8 +12,16 @@ $head = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $head["title"] ?></title>
+    <script src="js/zoom.js"></script>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
+    <img src="img/fish.jpg" alt="" class="zoom">
+    <img src="img/fish.jpg" alt="" class="zoom">
+    <img src="img/Menu.png" alt="" class="zoom">
+
+    <img src="img/1.webp" alt="" class="zoom" width="100">
     
 </body>
 </html>
