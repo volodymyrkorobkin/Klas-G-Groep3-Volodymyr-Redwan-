42756b30af773ed59608844596ac3e604bf42e01
volodymyrkorobkin <6040683@mborijnland.nl>
2024-02-13 11:19:02 +0100

    Initial files for the project

---
+++ pages/home.php
@@ -0,0 +1,19 @@
+<?php
+$PATH = "./";
+
+$head = [
+    "title" => "Home"
+];
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $head["title"] ?></title>
+</head>
+<body>
+    
+</body>
+</html>
