12efaa921bcc453e8c1dd01753e9cc3f89337e13
volodymyrkorobkin <6040683@mborijnland.nl>
2024-03-09 20:31:03 +0100

    Fix table border-spacing and remove unnecessary PHPMailer imports

--- php/email.php
+++ php/email.php
@@ -1,11 +1,8 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
 require $PATH . 'vendor/autoload.php';
-
 
 
 
