f800b7bbc7b3fdd9c7df189889a782fd63741fc1
volodymyrkorobkin <6040683@mborijnland.nl>
2024-03-09 14:40:37 +0100

    phpmailer files added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'user/fish-restaurant',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '279a91e7f889a45ffc11023d2cc6195d1f275201',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.1',
+            'version' => '6.9.1.0',
+            'reference' => '039de174cd9c17a8389754d3b877a2ed22743e18',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'user/fish-restaurant' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '279a91e7f889a45ffc11023d2cc6195d1f275201',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
