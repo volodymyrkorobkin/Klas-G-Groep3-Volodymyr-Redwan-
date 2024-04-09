a101f95b1aae0ec29c79c50692d3df3067c6c93b
volodymyrkorobkin <6040683@mborijnland.nl>
2024-04-09 13:23:36 +0200

    Fix sticky header behavior with opered dropdown

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const header = document.querySelector('header');
     const body = document.querySelector('body');
+    const dropdown = document.querySelector('#openDropDown');
     
     let headerPos = 0;
 
 
     body.style.paddingTop = header.offsetHeight + 'px';
     header.style.marginTop = '-' + header.offsetHeight + 'px';
+
     
 
     window.addEventListener('scroll', function(e) {
@@ -21,10 +23,15 @@ document.addEventListener('DOMContentLoaded', function() {
             if ((window.scrollY > header.offsetHeight * 1.5 || header.classList.contains('sticky')) && !checkBox.checked) {
                 header.style.position = 'fixed';
                 header.style.top = '-' + (parseInt(header.offsetHeight) + 10)  + 'px'
+                dropdown.querySelector(".dropdown").style.display = 'none';           
                 
                 this.setTimeout(function() {
                     header.classList.add('sticky');
                 }, 1);
+
+                this.setTimeout(function() {
+                    dropdown.querySelector(".dropdown").style.display = null;
+                }, 100);
             }
         }
 
